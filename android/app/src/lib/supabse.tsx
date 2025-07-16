bf70051151a25cf746a749bc6a6bfa37743d46b3
dilaksh06 <kamalathasandilakshan@gmail.com>
2025-07-16 21:13:00 +0530

    removed

--- android/app/src/lib/supabse.tsx
+++ android/app/src/lib/supabse.tsx
@@ -1,6 +1,5 @@
-const SUPABASE_URL = 'https://gpauttseecilvbnwylhs.supabase.co';
-const SUPABASE_ANON_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwYXV0dHNlZWNpbHZibnd5bGhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyMTUzOTEsImV4cCI6MjA2Njc5MTM5MX0.l-rjXco63HqjZdV1DD3qG1JF4zxDrf5SCVe35Hnlgdc';
+const SUPABASE_URL = 'https://abcc.supabase.co'; // add your url
+const SUPABASE_ANON_KEY = ''  // add your key
 
 export async function insertLocation(loc: {
     latitude: number;
